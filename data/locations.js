90572fb870c1d9398aa837a1f41a9af000afeed6
YoinkyYoinkers <111921674+YoinkyYoinkers@users.noreply.github.com>
2022-08-23 22:05:17 +0100

    Add files via upload

---
+++ data/locations.js
@@ -0,0 +1,291 @@
+var locations = [
+    {
+        "id": 0,
+        "description": "Barge at Francis International Airport",
+        "coordinates": [
+            2259,
+            937
+        ]
+    },
+    {
+        "id": 1,
+        "description": "Location of the Sultan RS",
+        "coordinates": [
+            -975,
+            1911
+        ]
+    },
+    {
+        "id": 2,
+        "description": "Swingset Glitch",
+        "coordinates": [
+            1345,
+            -240
+        ]
+    },
+    {
+        "id": 3,
+        "description": "Toilets at Middle Park",
+        "coordinates": [
+            -240,
+            740
+        ]
+    },
+    {
+        "id": 4,
+        "description": "Alderney State Correctional Facility",
+        "coordinates": [
+            -1051,
+            -447
+        ]
+    },
+    {
+        "id": 5,
+        "description": "Car Wash at Alderney",
+        "coordinates": [
+            -1362,
+            5
+        ]
+    },
+    {
+        "id": 6,
+        "description": "Jimmy Pegorino's House",
+        "coordinates": [
+            -1394,
+            1436
+        ]
+    },
+    {
+        "id": 7,
+        "description": "Police Station at Francis International Airport",
+        "coordinates": [
+            2219,
+            406
+        ]
+    },
+    {
+        "id": 8,
+        "description": "Binoculars at 'Funland'",
+        "coordinates": [
+            892,
+            -634
+        ]
+    },
+    {
+        "id": 9,
+        "description": "Archway in Outlook, Broker",
+        "coordinates": [
+            1101,
+            -58
+        ]
+    },
+    {
+        "id": 10,
+        "description": "Docks on Charge Island",
+        "coordinates": [
+            412,
+            1114
+        ]
+    },
+    {
+        "id": 11,
+        "description": "Skate Park in Algonquin",
+        "coordinates": [
+            -764,
+            141
+        ]
+    },
+    {
+        "id": 12,
+        "description": "Entrance to the driving range",
+        "coordinates": [
+            -529,
+            16
+        ]
+    },
+    {
+        "id": 13,
+        "description": "Graveyard on Colony Island",
+        "coordinates": [
+            468.88,
+            304.47
+        ]
+    },
+    {
+        "id": 14,
+        "description": "Suite in the Majestic Hotel",
+        "coordinates": [
+            -172,
+            599
+        ]
+    },
+    {
+        "id": 15,
+        "description": "Burger Shot at Star Junction",
+        "coordinates": [
+            -217,
+            244
+        ]
+    },
+    {
+        "id": 16,
+        "description": "West Dyke Hospital",
+        "coordinates": [
+            -1283,
+            1288
+        ]
+    },
+    {
+        "id": 17,
+        "description": "Little Jacob's Hangout",
+        "coordinates": [
+            1475,
+            62
+        ]
+    },
+    {
+        "id": 18,
+        "description": "Reception in the Happiness Island Statue",
+        "coordinates": [
+            -608,
+            -792
+        ]
+    },
+    {
+        "id": 19,
+        "description": "60 Diner in Algonquin",
+        "coordinates": [
+            -389,
+            244
+        ]
+    },
+    {
+        "id": 20,
+        "description": "Burger Shot in Broker",
+        "coordinates": [
+            1588,
+            203
+        ]
+    },
+    {
+        "id": 21,
+        "description": "Highway Junction in Bohan",
+        "coordinates": [
+            650,
+            1373
+        ]
+    },
+    {
+        "id": 22,
+        "description": "Burger Shot in Bohan",
+        "coordinates": [
+            1114,
+            1636
+        ]
+    },
+    {
+        "id": 23,
+        "description": "Park in Bohan",
+        "coordinates": [
+            464,
+            1920
+        ]
+    },
+    {
+        "id": 24,
+        "description": "Gas Station in Bohan",
+        "coordinates": [
+            1422,
+            1745
+        ]
+    },
+    {
+        "id": 25,
+        "description": "69th Diner in Hove Beach",
+        "coordinates": [
+            898,
+            -460
+        ]
+    },
+    {
+        "id": 26,
+        "description": "A Street in Meadow Hills",
+        "coordinates": [
+            1656,
+            721
+        ]
+    },
+    {
+        "id": 27,
+        "description": "Internet Cafe in Outlook",
+        "coordinates": [
+            994,
+            -158
+        ]
+    },
+    {
+        "id": 28,
+        "description": "Just Outside of the Algonquin HeliTour",
+        "coordinates": [
+            304,
+            -693
+        ]
+    },
+    {
+        "id": 29,
+        "description": "Church in Hatton Gardens",
+        "coordinates": [
+            -25,
+            376
+        ]
+    },
+    {
+        "id": 30,
+        "description": "Grotti Dealership in Middle Park East",
+        "coordinates": [
+            30,
+            831
+        ]
+    },
+    {
+        "id": 31,
+        "description": "Train Station in Hove Beach",
+        "coordinates": [
+            979,
+            -521
+        ]
+    },
+    {
+        "id": 32,
+        "description": "Brucie's Executive Lifestyle Autos",
+        "coordinates": [
+            843,
+            -102
+        ]
+    },
+    {
+        "id": 33,
+        "description": "The Libertonian Museum",
+        "coordinates": [
+            -37,
+            793
+        ]
+    },
+    {
+        "id": 34,
+        "description": "24 Hour Parking Garage in Algonquin",
+        "coordinates": [
+            -593,
+            478
+        ]
+    },
+    {
+        "id": 35,
+        "description": "Algonquin Side of Broker Bridge",
+        "coordinates": [
+            456,
+            -451
+        ]
+    }
+]
+
